d8e4865f835e845d02af2220a223ab11d7f3b726
A-Givertzman <a.givertzman@icloud.com>
2026-07-15 12:37:25 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,23 +1,30 @@
 #!/bin/bash
 GITHUB_USER="a-givertzman"
 
-# 1. Установка утилит и официального бинарника Hysteria
-apt update && apt install -y curl openssl
-bash <(curl -fsSL https://get.hy2.sh/)
-
-# 2. Создание директорий и генерация самоподписанного SSL-сертификата
+# 1. Полная очистка старых конфигов и создание чистой папки
+rm -rf /etc/hysteria
 mkdir -p /etc/hysteria
-openssl req -x509 -nodes -newkey rsa:2048 -keyout /etc/hysteria/server.key -out /etc/hysteria/server.crt -days 3650 -subj "/CN=://microsoft.com"
 
-# Исправление прав для Debian 12 (чтобы служба Hysteria могла прочесть ключи)
+# 2. Установка необходимых системных утилит
+apt update && apt install -y curl openssl
+
+# 3. Установка официального сервера Hysteria 2 (от root)
+HYSTERIA_USER=root bash <(curl -fsSL https://get.hy2.sh/)
+
+# 4. Гарантированная автоматическая генерация ОБОИХ файлов ключей без вопросов (-batch)
+mkdir -p /etc/hysteria
+openssl req -x509 -nodes -days 3650 -newkey rsa:2048 -keyout /etc/hysteria/server.key -out /etc/hysteria/server.crt -subj "/CN=://microsoft.com" -batch
+
+# 5. Исправление прав для Debian 12 (чтобы служба Hysteria могла прочесть ключи)
 chown -R hysteria:hysteria /etc/hysteria
 chmod 644 /etc/hysteria/server.crt
 chmod 600 /etc/hysteria/server.key
 
-# 3. Скачивание конфига с вашего GitHub
+# 6. Скачивание конфига с вашего GitHub
 curl -o /etc/hysteria/config.yaml https://raw.githubusercontent.com/a-givertzman/rem-conf/refs/heads/master/config.yaml
 
-# 4. Настройка автозапуска и старт сервиса
+# 7. Настройка автозапуска и старт сервиса
+systemctl daemon-reload
 systemctl enable hysteria-server.service
 systemctl restart hysteria-server.service
 
